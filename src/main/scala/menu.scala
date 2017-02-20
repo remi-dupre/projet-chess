8c4cddaba36c762089c38b0146f2635eb69fea17
Rémi Dupré <remim.dupre@gmail.com>
2017-02-20 08:50:28 +0100

    Ajout des autres boutons

--- src/main/scala/menu.scala
+++ src/main/scala/menu.scala
@@ -2,15 +2,31 @@ import swing._
 
 class MenuWin extends MainFrame {
 	title = "test"
-	contents = new Button(Action("Start") {
-		println("Starting the game")
-		visible = false
-
-		val fen = new GameWin()
-		fen.game.players(0) = new Human(0, fen, fen.game)
-		fen.game.players(1) = new Human(1, fen, fen.game)
-		fen.game.start
-		fen.visible = true
-	})
+	contents = new BoxPanel(Orientation.Vertical) {
+		contents += new Button(Action("Joueur vs. Joueur") {
+			visible = false
+			val fen = new GameWin()
+			fen.game.players(0) = new Human(0, fen, fen.game)
+			fen.game.players(1) = new Human(1, fen, fen.game)
+			fen.game.start
+			fen.visible = true
+		})
+		contents += new Button(Action("IA vs. Joueur") {
+			visible = false
+			val fen = new GameWin()
+			fen.game.players(0) = new Human(0, fen, fen.game)
+			fen.game.players(1) = new IA(1, fen.game)
+			fen.game.start
+			fen.visible = true
+		})
+		contents += new Button(Action("IA vs. IA") {
+			visible = false
+			val fen = new GameWin()
+			fen.game.players(0) = new IA(0, fen.game)
+			fen.game.players(1) = new IA(1, fen.game)
+			fen.game.start
+			fen.visible = true
+		})
+	}	
 }
 
